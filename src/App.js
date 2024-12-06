412e3c6b01b2e40e8007fbccd053ba5f125bdaf6
reda <redabouchi42@gmail.com>
2024-12-06 22:14:33 +0100

    crud-react

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import AddUser from './Composants/AddUser';
+import UpdateUser from './Composants/UpdateUser';
+import UserList from './Composants/UserList';
+function App() {
+  return (
+    <div style={{paddingLeft:40}}>
+      <h1>CRUD REACT-REDUX Example 1</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<UserList />} />
+          <Route path='/add-user' element={<AddUser />} />
+          <Route path='/update-user/:id' element={<UpdateUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
